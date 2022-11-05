2b455c187b58df9ebf5a6ed4f3dc6944298afeaf
Pixel <nikodems1308@gmail.com>
2022-11-05 19:37:18 +0100

    hjghh

--- Sidebar/sidebar.js
+++
@@ -1,7 +0,0 @@
-// classify button and content
-let btn = document.querySelector('.right-section-hamburgermenu')
-let content = document.querySelector('.hamburger-menu')
-// add content to button
-btn.addEventListener ('click', function(){
-content.classList.toggle('hamb-menu-opacity-1')
-})
