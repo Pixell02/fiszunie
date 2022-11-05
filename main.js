cc6bc234197222e1752de5eb8b0a3b770e7005a2
Pixell02 <101512918+Pixell02@users.noreply.github.com>
2022-11-05 20:32:22 +0100

    Add files via upload
    pliczki

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+// classify button and content
+let btn = document.querySelector('.right-section-hamburgermenu')
+let content = document.querySelector('.hamburger-menu')
+// add content to button
+btn.addEventListener ('click', function(){
+content.classList.toggle('hamb-menu-opacity-1')
+})
+
+// Adding cards contents to observe
+
+const cards = document.querySelectorAll('.about-project-container')
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        const aboutProject = entry.target.querySelector('.about-project-container');
-        const title = entry.target.querySelector('.title');
-        if (entry.isIntersecting) {
-            
-            aboutProject.classList.add('transition');
-            
-            return;
-        }
-        aboutProject.classList.remove('transition');
-        
-
+        entry.target.classList.toggle('transition', entry.isIntersecting)
     });
 });
 
-observer.observe(document.querySelector('.project-cont'));
+cards.forEach(card => {
+    observer.observe(card)
+})
